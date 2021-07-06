990f210fc1cc39a7c0a7c4e6d7e7e7f2d20a7144
JulianRaso <julianraso@live.com>
2021-07-06 18:25:38 -0300

    Adding points & more style

--- src/rps.js
+++ src/rps.js
@@ -1,33 +1,71 @@
+const status = document.getElementById("roundStatus");
+const winUser = document.getElementById('user');
+const winPc = document.getElementById('pc');
+var userPoints = 0;
+var pcPoints = 0;
+
 
 function computerPlay(){
     options = ["Rock","Paper","Scissors"];
     return(options[Math.floor(Math.random()*3)]);
 }
 
-function play(playerSelection,computerSelection){
 
-    if(playerSelection == "Rock"){
+function playRound(playerSelection,computerSelection){
+
+
+    if (playerSelection == computerSelection){
+        return ("Tie Game")
+    }
+    else if(playerSelection == "Rock"){
         if(computerSelection == "Paper"){
-            console.log("You Lose!" + computerSelection + "beats" + playerSelection);
+            return("You Lose! " + computerSelection + " beats " + playerSelection);
         }
-        else {
-            console.log("You Win!" + playerSelection + "beats" + computerSelection );
+        else if (computerSelection == "Scissors"){
+            return("You Win! " + playerSelection + " beats " + computerSelection );
         }
     }
     else if(playerSelection == "Paper"){
         if(computerSelection == "Rock"){
-            console.log("You Win!" + computerSelection + "beats" + playerSelection);
+            return("You Win! " + computerSelection + " beats " + playerSelection);
         }
-        else {
-            console.log("You Lose!" + playerSelection + "beats" + computerSelection );
+        else if(computerSelection == "Scissors"){
+            return("You Lose! " + playerSelection + " beats " + computerSelection );
         }
     }
     else if(playerSelection == "Scissors"){
         if(computerSelection == "Paper"){
-            console.log("You Win!" + computerSelection + "beats" + playerSelection);
+            return("You Win! " + computerSelection + " beats " + playerSelection);
         }
-        else {
-            console.log("You Lose!" + playerSelection + "beats" + computerSelection );
+        else if (computerSelection == "Rock") {
+            return("You Lose! " + playerSelection + " beats " + computerSelection );
         }
     }
+}
+
+function play(selection){
+    if(userPoints == 3 || pcPoints == 3 ){
+        userPoints = 0;
+        pcPoints = 0;
+    }
+    return status.innerHTML = playRound(selection,computerPlay());
+}
+
+function game(btn){
+    var playStatus = play(btn.id);
+
+    console.log(playStatus.includes('Lose'));
+    
+    if(playStatus.includes('Win') == true){
+        userPoints = userPoints + 1;
+        winUser.innerHTML = ("You = " + userPoints)
+        winPc.innerHTML = ("Computer = " + pcPoints )  
+    }
+    else if (playStatus.includes('Lose') == true){
+        pcPoints = pcPoints + 1;
+        winUser.innerHTML = ("You = " + userPoints)
+        winPc.innerHTML = ("Computer = " + pcPoints )  
+    }
+    else {}
+
 }
